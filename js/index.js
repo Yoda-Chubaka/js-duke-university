39f286df93a5f9569b6fcb6bf52f8f0c4f2c26b2
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-01-18 18:16:33 +0200

    New commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 function dochange() {
     alert("clicked button with JS function");
 }
+
+function doconfirm() {
+    let message;
+    if (confirm("clicked button with JS function") == true) {
+        message = "You pressed OK!";
+    }
+    else {
+        message = "Are you sure you want to cancel?";
+    }
+    document.getElementById("demo").innerHTML = message;
+}
