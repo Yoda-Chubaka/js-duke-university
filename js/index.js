50e4f3172ddab4945989e6cdda30f961a1e3286e
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-01-19 17:23:49 +0200

    New commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,38 @@
-function dochange() {
+function doChange() {
     alert("clicked button with JS function");
 }
 
-function doconfirm() {
-    let message;
+function doConfirm() {
+    var message;
     if (confirm("clicked button with JS function") == true) {
-        message = "You pressed OK!";
+        message = alert("You pressed OK!");
     }
     else {
-        message = "Are you sure you want to cancel?";
+        message = alert("Are you sure you want to cancel?");
     }
-    document.getElementById("demo").innerHTML = message;
+    // document.getElementById("demo").innerHTML = message;
+}
+
+function changeColor() {
+    var colorBox1 = document.getElementById("card1");
+    var colorBox2 = document.getElementById("card2");
+    var colorBox3 = document.getElementById("card3");
+    var colorBox4 = document.getElementById("card4");
+
+    colorBox1.className = "viseVersaColor";
+    colorBox2.className = "viseVersaColor";
+    colorBox3.className = "viseVersaColor";
+    colorBox4.className = "viseVersaColor";
+}
+
+function changeText() {
+    var textBox1 = document.getElementById("card1");
+    var textBox2 = document.getElementById("card2");
+    var textBox3 = document.getElementById("card3");
+    var textBox4 = document.getElementById("card4");
+
+    textBox1.innerHTML = "Hello world! It's my first JS function😎";
+    textBox2.innerHTML = "Hello world! It's my first JS function😎";
+    textBox3.innerHTML = "Hello world! It's my first JS function😎";
+    textBox4.innerHTML = "Hello world! It's my first JS function😎";
 }
