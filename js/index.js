f7cf947594f0a8786846fcd182f05992ae175af0
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-01-24 17:45:25 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -118,4 +118,53 @@ function doSquare() {
     ctx.clearRect(0, 0, dd1.width, dd1.height);
     ctx.fillStyle="yellow";
     ctx.fillRect(10, 10, size, size);
+    ctx.fillStyle="green";
+    ctx.fillRect(parseInt(size)+20,10,size,size);
+}
+
+
+
+
+
+var paintcanvas = document.getElementById("canvas1");
+var context = paintcanvas.getContext("2d");
+var color = 'black';
+var radius = 50;
+// малюватиме, лише якщо натискати на мишку (спрацьовує лише з натиснутою кнопкою)
+var isPainting = false;
+
+function setWidth(value) {
+    if (isNumeric() === true) {
+        canvas.width = value;
+    }
+}
+
+function setHeight(value) {
+    if (isNumeric === true) {
+        canvas.height = value;
+    }
+}
+
+function clearCanvas() {
+    context.clearRect(0, 0, paintcanvas.width, paintcanvas.height);
+}
+
+function paintCircle(x, y) {
+    // щоразу починайте нове коло (circle)
+    context.beginPath();
+    // малюйте коло з радіусом 2*PI навколо заданої точки
+    context.arc(x, y, radius, 0, Math.PI * 2, true);
+    context.fillStyle = color;
+    context.fill();
+}
+
+// перевірте, щоб value було числовим значенням
+function isNumeric(value) {
+    if (Number.isInteger(value)) {
+        return true;
+    }
+    else {
+    // стандартна функція JavaScript, аби визначити, чи рядок можна перетворити рядок на число 
+    return !isNaN(value);
+    }
 }
